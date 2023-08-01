6fd8005499a8dd0b81ec907f81ec61731646d284
Tobi <107647385+TurkiNAlghamdii@users.noreply.github.com>
2023-08-01 20:31:15 +0300

    Add cleaned code and added comments

--- index.js
+++ index.js
@@ -1,46 +1,40 @@
-function getComputerChoice(){
-    let number = Math.floor(Math.random() * (3 - 1 + 1) + 1)
-    let choice;
-    if (number===1){
-        choice = 'rock'
-    }else if (number ===2){
-        choice = 'paper'
-    }else {
-        choice = 'scissors'
-    }
-    return choice
+// Function for a random number between 1 and 3 and assigning each number to either rock paper or scissors
+function getComputerChoice() {
+  let number = Math.floor(Math.random() * (3 - 1 + 1) + 1);
+  let choice;
+  if (number === 1) {
+    choice = "rock";
+  } else if (number === 2) {
+    choice = "paper";
+  } else {
+    choice = "scissors";
+  }
+  return choice;
 }
 
-function playRound(user,computer){
-    console.log(computer);
-    let result;
-    // let userChoice;
-    // if(user.toLowerCase()==='rock'){
-    //     userChoice = 1
-    // }else if (user.toLowerCase()==='scissors'){
-    //     userChoice = 2
-    // }else{
-    //     userChoice = 3
-    // }
-    let userChoice = user.toLowerCase()
-    console.log(userChoice);
-    if (userChoice===computer) {
-        return "It's a tie!"
-    }else if(userChoice ==="rock" && computer ==="paper"){
-        return "Computer wins!!"
+// A function to play a round with computer and user inputs
+function playRound(user, computer) {
+  console.log(computer);
+  let result;
 
-    }else if(userChoice ==="rock" && computer ==="scissors"){
-        return "Human wins!!"
-    }else if(userChoice ==="paper" && computer ==="scissors"){
-        return "computer wins!!"
-    }else if(userChoice ==="paper" && computer ==="rock"){
-        return "Human wins!!"
-    }else if(userChoice ==="scissors" && computer ==="rock"){
-        return "Computer wins!!"
-    }else if(userChoice ==="scissors" && computer ==="paper"){
-        return "Human wins!!"
-    }
+  let userChoice = user.toLowerCase(); // To let user input without any case sensitivity
+
+  if (userChoice === computer) {
+    return "It's a tie!";
+  } else if (userChoice === "rock" && computer === "paper") {
+    return "Computer wins!!";
+  } else if (userChoice === "rock" && computer === "scissors") {
+    return "Human wins!!";
+  } else if (userChoice === "paper" && computer === "scissors") {
+    return "computer wins!!";
+  } else if (userChoice === "paper" && computer === "rock") {
+    return "Human wins!!";
+  } else if (userChoice === "scissors" && computer === "rock") {
+    return "Computer wins!!";
+  } else if (userChoice === "scissors" && computer === "paper") {
+    return "Human wins!!";
+  }
 }
-// console.log(getComputerChoice());
 
-console.log(playRound("paper", getComputerChoice()))
+
+console.log(playRound("paper", getComputerChoice()));
